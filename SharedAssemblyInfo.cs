ed49651c1e5086bd38d660fb8ee0e15230f075a1
tom-englert <mail@tom-englert.de>
2016-01-30 17:33:59 +0100

    V 1.0.1.0

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -5,8 +5,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("tom-englert.de")]
 [assembly: AssemblyProduct("Project Configuration Manager")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("© 2016, tom-englert.de")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
--- Version.cs
+++ Version.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 [assembly: AssemblyVersion(Product.Version)]
 [assembly: AssemblyFileVersion(Product.Version)]
 
-class Product
+internal static class Product
 {
-    public const string Version = "1.0.0.0";
+    public const string Version = "1.0.1.0";
 }
