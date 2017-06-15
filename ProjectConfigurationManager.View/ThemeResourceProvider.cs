19612d2051924bd06927646430f3632a3ab26a12
tom-englert <mail@tom-englert.de>
2017-06-15 17:39:08 +0200

    Use styles in BuildConfigurationView

---
+++ ProjectConfigurationManager.View/ThemeResourceProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace tomenglertde.ProjectConfigurationManager.View
+{
+    using System.ComponentModel.Composition;
+    using System.Windows;
+
+    using TomsToolbox.Wpf.Styles;
+
+    [Export(typeof(IThemeResourceProvider))]
+    internal class ThemeResourceProvider : IThemeResourceProvider
+    {
+        public void LoadThemeResources(ResourceDictionary resource)
+        {
+        }
+    }
+}
